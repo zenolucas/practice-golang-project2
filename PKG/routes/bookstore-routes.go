8e4ff494ff55370a06e6c21c0bd65aeb897733f6
Zen Lucas <2200501@slu.edu.ph>
2023-01-30 06:23:43 +0800

    create routes

--- PKG/routes/bookstore-routes.go
+++ PKG/routes/bookstore-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/zenolucas/practice-golang-project2/PKG/controllers"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controllers.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controllers.DeleteBook).Methods("DELETE")
+}
