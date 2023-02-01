5205897f19df0d03a8dbfccbf0f14a333bb0fb90
Zen Lucas <2200501@slu.edu.ph>
2023-02-01 20:01:10 +0800

    in book-controller.go, created functions CreateBook() and DeleteBook

--- PKG/controllers/book-controller.go
+++ PKG/controllers/book-controller.go
@@ -44,3 +44,17 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
 }
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	bookId := vars["bookId"]
+	ID, err := strconv.ParseInt(bookId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	book := models.DeleteBook(ID)
+	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "pkglication/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
